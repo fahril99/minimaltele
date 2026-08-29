11b1683e8ef27d6e3029fadb413364998f068b87
fahril99 <jed832615@gmail.com>
2026-08-29 20:14:37 +0700

    fix: Kotlin compile errors (encoding and missing import)

--- app/src/main/java/com/minimaltelegram/PhotoViewerActivity.kt
+++ app/src/main/java/com/minimaltelegram/PhotoViewerActivity.kt
@@ -2,6 +2,7 @@ package com.minimaltelegram
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
